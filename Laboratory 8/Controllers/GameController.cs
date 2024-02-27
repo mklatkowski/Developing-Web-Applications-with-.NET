773d608e55178dd019113cbf6c96e70cc5043b55
michal.klatkowski <49809012+mklatkowski@users.noreply.github.com>
2024-02-27 17:43:41 +0100

    MVC introduction
    Usages of controllers - simple game controller and quadratic formula equation solver controller with session variables

---
+++ Laboratory 8/Controllers/GameController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Lab08_MVC.Controllers
+{
+    public class GameController : Controller
+    {
+        private const string RangeSessionKey = "Range";
+        private const string GuessCountSessionKey = "GuessCount";
+        private const string RandValueSessionKey = "RandValue";
+
+        public IActionResult Guess([FromRoute] int value)
+        {
+            int range = GetSessionInt(RangeSessionKey);
+            int guessCount = GetSessionInt(GuessCountSessionKey);
+            int randValue = GetSessionInt(RandValueSessionKey);
+
+            guessCount++;
+            if (value == randValue)
+            {
+                ViewBag.info = $"Odgadnio liczbę w {guessCount} próbach";
+                guessCount = 0;
+            }
+            else if (value > randValue)
+            {
+                ViewBag.info = $"Poszukiwana liczba jest mniejsza niż {value}";
+            }
+            else
+            {
+                ViewBag.info = $"Poszukiwana liczba jest większa niż {value}";
+            }
+
+            SetSessionInt(GuessCountSessionKey, guessCount);
+            ViewBag.guessCount = guessCount;
+
+            return View();
+        }
+
+        public IActionResult Set([FromRoute] int n)
+        {
+            SetSessionInt(RangeSessionKey, n);
+            SetSessionInt(GuessCountSessionKey, 0);
+            SetSessionInt(RandValueSessionKey, randValueToGuess(n));
+
+            ViewBag.info = $"Ustawiono zakres na {n}.";
+            ViewBag.guessCount = 0;
+            ViewBag.range = n;
+
+            return View();
+        }
+
+        public IActionResult Draw()
+        {
+            int range = GetSessionInt(RangeSessionKey);
+            SetSessionInt(RandValueSessionKey, randValueToGuess(range));
+            SetSessionInt(GuessCountSessionKey, 0);
+
+            ViewBag.guessCount = 0;
+            ViewBag.info = $"Wylosowano liczbę z zakresu 0-{range - 1}";
+
+            return View();
+        }
+
+        private int randValueToGuess(int range)
+        {
+            Random rand = new Random();
+            return rand.Next(0, range);
+        }
+
+        private int GetSessionInt(string key)
+        {
+            return HttpContext.Session.GetInt32(key) ?? 0;
+        }
+
+        private void SetSessionInt(string key, int value)
+        {
+            HttpContext.Session.SetInt32(key, value);
+        }
+    }
+}
---
+++ Laboratory 8/Controllers/ToolController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Lab08_MVC.Controllers
+{
+    public class ToolController : Controller
+    {
+        public IActionResult Solve(double a, double b, double c) {
+
+            ViewBag.a = a;
+            ViewBag.b = b;
+            ViewBag.c = c;
+
+            ViewBag.result = GetResults(a, b, c);
+
+            setClass(ViewBag.result);
+
+            return View();
+        }
+
+        private void setClass(List<double> list)
+        {
+            if(list == null)
+            {
+                ViewBag.resultClass = "infinity";
+                ViewBag.info = "Nieskończenie wiele rozwiązań";
+            }
+            else if(list.Count == 0) {
+                ViewBag.resultClass = "zero-solutions";
+                ViewBag.info = "Nie ma rozwiązań";
+            }
+            else if(list.Count == 1)
+            {
+                ViewBag.resultClass = "one-solution";
+                ViewBag.info = "Jest 1 rozwiązanie";
+            }
+            else
+            {
+                ViewBag.resultClass = "two-solutions";
+                ViewBag.info = "Są 2 rozwiązania";
+            }
+        }
+
+        private List<double> GetResults(double a, double b, double c)
+        {
+            List<double> results = new List<double>();
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    if (c == 0)
+                    {
+                        results = null;
+                    }
+                    else
+                    {
+                    }
+
+                }
+                else
+                {
+                    results.Add(-c / b);
+                }
+            }
+            else
+            {
+                double delta = Math.Pow(b, 2) - 4 * a * c;
+                if (delta > 0)
+                {
+                    double result1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    double result2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    results.Add(result1);
+                    results.Add(result2);
+                }
+                else if (delta == 0)
+                {
+
+                    results.Add(-b / (2 * a));
+                }
+                else
+                {
+                }
+            }
+            return results;
+        }
+    }
+}

ac22798824a1b914be95fb35025438a0630cc210
mklatkowski <michalklatkowski15@gmail.com>
2024-03-02 15:17:47 +0100

    Entity Framework extension

---
+++ Laboratory 10/List10Csharp/Controllers/ShopController.cs
@@ -0,0 +1,46 @@
+﻿// ShopController.cs
+
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using List10Csharp.Data;
+
+namespace List10Csharp.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ShopDbContext _context;
+
+        public ShopController(ShopDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Shop
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+        // GET: Shop/Products/5
+        public IActionResult Products(int? categoryId)
+        {
+            if (categoryId == null)
+            {
+                return NotFound();
+            }
+
+            var category = _context.Categories
+                .Include(c => c.Articles)
+                .FirstOrDefault(c => c.Id == categoryId);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+    }
+}
---
+++ Laboratory 10/List10Csharp/Data/ShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using List10Csharp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace List10Csharp.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
